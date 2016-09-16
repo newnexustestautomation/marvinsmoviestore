1eac01dab631b5314ee66703f292c0343d94bf43
Marvin Leito <m.leito@gmail.com>
2016-09-16 12:53:35 +0200

    Windows10 geinstalleerd, applicationhost en batch file aangepast.

---
+++ IIS_Express.bat
@@ -0,0 +1,3 @@
+start cmd.exe
+cd "c:\Program Files (x86)\IIS Express\"
+start iisexpress /config:C:\dev\CheckIT_DOTNET\marvinsmoviestore\.vs\config\applicationhost.config /site:marvinsmoviestore.Web(2)
